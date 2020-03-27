8485b4108f6d8d5b056a700cfb5d39e7cf25a274
Mehdi <medirz90@icloud.com>
2020-03-26 23:25:21 -0400

    run mocks

--- eboss_mocks.bash
+++ eboss_mocks.bash
@@ -4,6 +4,8 @@
 eval "$(/home/mehdi/miniconda3/bin/conda shell.bash hook)"
 conda activate py3p6
 
+export NUMEXPR_MAX_THREADS=2
+
 ablation=${HOME}/github/LSSutils/scripts/analysis/ablation_tf_old.py
 multfit=${HOME}/github/LSSutils/scripts/analysis/mult_fit.py
 nnfit=${HOME}/github/LSSutils/scripts/analysis/nn_fit_tf_old.py
@@ -16,41 +18,109 @@ pkracut=${HOME}/github/LSSutils/scripts/analysis/run_pk_racut.py
 
 # --- prepare for NN regression
 # took 2 min
+nside=512
 cap=NGC
 path_mocks=/B/Shared/eBOSS
 systematics_name=/home/mehdi/data/templates/SDSS_WISE_HI_imageprop_nside512.h5
 
+axfit0='0 1'
+axfit1='0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19'
 
-#--- mock 1 is done separately for debugging purposes
-for i in $(seq -f "%04g" 2 9)
+
+# for i in $(seq -f "%04g" 1 9)
+# do
+
+#     for cont in contaminated null
+#     do
+    
+#         if [ ${cont} == 'contaminated' ]
+#         then
+#             data_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_${i}.dat.fits
+#             random_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_${i}.ran.fits
+#         else
+#             data_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_noweight_${i}.dat.fits
+#             random_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_noweight_${i}.ran.fits            
+#         fi
+    
+    
+    
+#         output_dir=/B/Shared/mehdi/eboss/mocks/${cap}_${i}_${cont}
+          
+#         echo ${i} $output_dir
+#         #du -h $data_name $random_name $systematics_name
+        
+#         # mocks do not have slice zhigh
+#         python prepare_data.py -d $data_name -r $random_name -s $systematics_name -o $output_dir \
+#                     -c $cap -sl low high all z1 z2 z3 --log log_${cont}_${i}.txt
+    
+    
+#     done
+# done
+
+
+# --- perform regression
+for i in $(seq -f "%04g" 1 9)
 do
 
     for cont in contaminated null
     do
-    
-        if [ ${cont} == 'contaminated' ]
-        then
-            data_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_${i}.dat.fits
-            random_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_${i}.ran.fits
-        else
-            data_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_noweight_${i}.dat.fits
-            random_name=${path_mocks}/${cont}/EZmock_eBOSS_QSO_${cap}_v7_noweight_${i}.ran.fits            
-        fi
-    
-    
-    
-        output_dir=/B/Shared/mehdi/eboss/mocks/${cap}_${i}_${cont}
-          
-        echo ${i} $output_dir
-        #du -h $data_name $random_name $systematics_name
-        
-        # mocks do not have slice zhigh
-        python prepare_data.py -d $data_name -r $random_name -s $systematics_name -o $output_dir \
-                    -c $cap -sl low high all z1 z2 z3 --log log_${cont}_${i}.txt
-    
-    
+       
+        for zcut in low high all z1 z2 z3
+        do
+
+            output_dir=/B/Shared/mehdi/eboss/mocks/${cap}_${i}_${cont}
+
+            ngal_features_5fold=${output_dir}/ngal_features_${cap}_${zcut}_${nside}.5r.npy
+
+            echo ${i} $output_dir
+            #du -h $data_name $random_name $systematics_name $ngal_features_5fold
+
+            #-- define output dirs
+            oudir_ab=${output_dir}/results/${cap}_${zcut}_${nside}/ablation/
+            oudir_reg=${output_dir}/results/${cap}_${zcut}_${nside}/regression/            
+
+            #-- define output names
+            log_ablation=eboss_mocks.log
+            nn1=nn_ablation
+            nn2=nn_plain           
+            nn3=nn_known
+
+            #du -h ${ngal_features_5fold}
+            #echo $oudir_ab
+            #echo $oudir_reg
+            
+
+           #-- ablation
+           for fold in 0 1 2 3 4
+           do
+               echo "feature selection on " $fold ${cap}_${zcut}
+               mpirun -np 16 python $ablation --data $ngal_features_5fold \
+                            --output $oudir_ab --log $log_ablation \
+                            --rank $fold --axfit $axfit1
+           done      
+
+           echo 'regression on ' $fold ${cap}_${zcut}
+
+           #-- regression with ablation
+           mpirun -np 5 python $nnfit --input $ngal_features_5fold \
+                               --output ${oudir_reg}${nn1}/ \
+                               --ablog ${oudir_ab}${log_ablation} --nside $nside
+
+           #-- regression with all maps
+           mpirun -np 5 python $nnfit --input $ngal_features_5fold \
+                              --output ${oudir_reg}${nn2}/ --nside $nside --axfit $axfit1 
+
+           #-- regression with known maps
+           mpirun -np 5 python $nnfit --input $ngal_features_5fold \
+                              --output ${oudir_reg}${nn3}/ --nside $nside --axfit $axfit0 
+
+
+        done        
     done
 done
+
+
+
 
 
 #
