68f08ff3dce21479c605d091032d65a1ebea2e4f
Mehdi <medirz90@icloud.com>
2020-04-22 20:53:21 -0400

    rerun data and mocks with nn aggreg

--- eboss_mocks.bash
+++ eboss_mocks.bash
@@ -127,34 +127,34 @@ axfit1='0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19'
 # ---- swap the weights in mock catalogs
 # NN's weights
 # 4 min
-for imock in {1..9}
-do 
-   for cont in null contaminated
-   do
-       for model in plain known ablation
-       do
-           for zsplit in lowmid all z3
-           do
-               if [ $zsplit == "lowmid" ]
-               then
-                   slices='low high'
-               elif [ $zsplit == "all" ]
-               then
-                   slices='all'
-               elif [ $zsplit == "z3" ]
-               then
-                   slices='z1 z2 z3'
-               else
-                   echo $zsplit 'not known'
-                   continue
-               fi
-               echo $imock $cont $model $zsplit $slices
-               python swap_mocks.py --cont ${cont} --imock ${imock} --model ${model} --zsplit ${zsplit} --slices ${slices}
-          done
-      done
-   done
-done
-#
+#for imock in {1..9}
+#do 
+#   for cont in null contaminated
+#   do
+#       for model in plain known ablation
+#       do
+#           for zsplit in lowmid all z3
+#           do
+#               if [ $zsplit == "lowmid" ]
+#               then
+#                   slices='low high'
+#               elif [ $zsplit == "all" ]
+#               then
+#                   slices='all'
+#               elif [ $zsplit == "z3" ]
+#               then
+#                   slices='z1 z2 z3'
+#               else
+#                   echo $zsplit 'not known'
+#                   continue
+#               fi
+#               echo $imock $cont $model $zsplit $slices
+#               python swap_mocks.py --cont ${cont} --imock ${imock} --model ${model} --zsplit ${zsplit} --slices ${slices}
+#          done
+#      done
+#   done
+#done
+##
 # Julian's code
 # 2 min
 #python do_systematics_fit.py QSO NGC 1 9 0.8 2.2
@@ -162,134 +162,130 @@ done
 
 
 #--- clustering
-#version=v7
-#nmesh=512
-#zlim=standard
-#
-#
-#for imock in $(seq -f "%04g" 8 9)
-#do 
-#
-#  if [ $zlim == "standard" ]
-#  then
-#      zrange='0.8 2.2'
-#  elif [ $zlim == "zhigh" ]
-#  then 
-#      zrange='2.2 3.5'
-#  elif [ $zlim == "combined" ]
-#  then
-#      zrange='0.8 3.5'
-#  fi
-#  
-#  for cont in null contaminated
-#  do
-#
-#
-#      #=================================
-#      # null and cont. with default correction
-#      echo "default method w "${cont}
-#      # default
-#      if [ ${cont} == "null" ]
-#      then
-#          galcat=/B/Shared/mehdi/eboss/mocks/NGC_${imock}_${cont}/EZmock_eBOSS_QSO_NGC_${version}_${imock}.dat.fits
-#          rancat=${galcat/.dat./.ran.}
-#
-#      elif [ ${cont} == "contaminated" ]
-#      then
-#          galcat=/B/Shared/eBOSS/contaminated/EZmock_eBOSS_QSO_NGC_${version}_${imock}.dat.fits
-#          rancat=${galcat/.dat./.ran.}
-#      fi
-#      du -h $galcat $rancat
-#      tag=QSO_NGC_${version}_${versiono}_systot_all_${imock}
-#      ouname=/B/Shared/mehdi/eboss/mocks/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
-#      echo $ouname
-#      mpirun -np 16 python $pk --galaxy_path $galcat \
-#                               --random_path $rancat \
-#                               --output_path $ouname \
-#                               --nmesh $nmesh --zlim ${zrange} --sys_tot
-#
-#      echo
-#      echo
-#      echo
-#
-#
-#
-#
-#      #=================================
-#      # cont without correction 
-#      if [ ${cont} == "contaminated" ]
-#      then
-#          galcat=/B/Shared/eBOSS/contaminated/EZmock_eBOSS_QSO_NGC_${version}_${imock}.dat.fits
-#          rancat=${galcat/.dat./.ran.}
-#          du -h $galcat $rancat
-#          tag=QSO_NGC_${version}_${versiono}_nowsys_all_${imock}
-#          ouname=/B/Shared/mehdi/eboss/mocks/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
-#          echo ${ouname}
-#
-#          mpirun -np 16 python $pk --galaxy_path $galcat \
-#                                   --random_path $rancat \
-#                                   --output_path $ouname \
-#                                   --nmesh $nmesh --zlim ${zrange}
-#      fi
-#      echo 
-#      echo
-#      echo
-#
-#
-#
-#
-#      #=================================
-#      # null without correction (truth)
-#      if [ ${cont} == "null" ]
-#      then
-#          galcat=/B/Shared/eBOSS/null/EZmock_eBOSS_QSO_NGC_${version}_noweight_${imock}.dat.fits
-#          rancat=${galcat/.dat./.ran.}
-#          du -h $galcat $rancat
-#          tag=QSO_NGC_${version}_${versiono}_truth_all_${imock}
-#          ouname=/B/Shared/mehdi/eboss/mocks/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
-#          echo ${ouname}
-#
-#          mpirun -np 16 python $pk --galaxy_path $galcat \
-#                                   --random_path $rancat \
-#                                   --output_path $ouname \
-#                                   --nmesh $nmesh --zlim ${zrange}
-#      fi
-#      echo 
-#      echo
-#      echo
-#      
-#      
-#
-#      #=================================
-#      echo "NN method w "${cont}
-#      # NN-based
-#      for model in plain known ablation
-#      do
-#           for zsplit in lowmid all z3
-#           do
-#               #echo $imock $cont $model $zsplit
-#               tag=QSO_NGC_${version}_${versiono}_${model}_${zsplit}_${imock}
-#               galcat=/B/Shared/mehdi/eboss/mocks/NGC_${imock}_${cont}/EZmock_eBOSS_${tag}.dat.fits
-#               rancat=${galcat/.dat./.ran.}
-#               ouname=/B/Shared/mehdi/eboss/mocks/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
-#
-#               du -h $galcat $rancat
-#               echo ${ouname}
-#
-#               mpirun -np 16 python $pk --galaxy_path $galcat \
-#                                       --random_path $rancat \
-#                                       --output_path $ouname \
-#                                       --nmesh $nmesh --zlim ${zrange} --sys_tot
-#            done
-#      done
-#      echo 
-#      echo
-#      echo
-#
-#
-#  done
-#done
-#
-##
-#
-#
+version=v7
+nmesh=512
+zlim=standard
+
+
+for imock in $(seq -f "%04g" 1 9)
+do 
+
+  if [ $zlim == "standard" ]
+  then
+      zrange='0.8 2.2'
+  elif [ $zlim == "zhigh" ]
+  then 
+      zrange='2.2 3.5'
+  elif [ $zlim == "combined" ]
+  then
+      zrange='0.8 3.5'
+  fi
+  
+  for cont in null contaminated
+  do
+
+
+      #=================================
+      # null and cont. with default correction
+      echo "default method w "${cont}
+      # default
+      if [ ${cont} == "null" ]
+      then
+          galcat=/B/Shared/mehdi/eboss/mocks/${versiono}/NGC_${imock}_${cont}/EZmock_eBOSS_QSO_NGC_${version}_${imock}.dat.fits
+          rancat=${galcat/.dat./.ran.}
+
+      elif [ ${cont} == "contaminated" ]
+      then
+          galcat=/B/Shared/eBOSS/contaminated/EZmock_eBOSS_QSO_NGC_${version}_${imock}.dat.fits
+          rancat=${galcat/.dat./.ran.}
+      fi
+      du -h $galcat $rancat
+      tag=QSO_NGC_${version}_${versiono}_systot_all_${imock}
+      ouname=/B/Shared/mehdi/eboss/mocks/${versiono}/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
+      echo $ouname
+      mpirun -np 16 python $pk --galaxy_path $galcat \
+                               --random_path $rancat \
+                               --output_path $ouname \
+                               --nmesh $nmesh --zlim ${zrange} --sys_tot
+
+      echo
+      echo
+      echo
+
+
+
+
+      #=================================
+      # cont without correction 
+      if [ ${cont} == "contaminated" ]
+      then
+          galcat=/B/Shared/eBOSS/contaminated/EZmock_eBOSS_QSO_NGC_${version}_${imock}.dat.fits
+          rancat=${galcat/.dat./.ran.}
+          du -h $galcat $rancat
+          tag=QSO_NGC_${version}_${versiono}_nowsys_all_${imock}
+          ouname=/B/Shared/mehdi/eboss/mocks/${versiono}/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
+          echo ${ouname}
+
+          mpirun -np 16 python $pk --galaxy_path $galcat \
+                                   --random_path $rancat \
+                                   --output_path $ouname \
+                                   --nmesh $nmesh --zlim ${zrange}
+      fi
+      echo 
+      echo
+      echo
+
+
+
+
+      #=================================
+      # null without correction (truth)
+      if [ ${cont} == "null" ]
+      then
+          galcat=/B/Shared/eBOSS/null/EZmock_eBOSS_QSO_NGC_${version}_noweight_${imock}.dat.fits
+          rancat=${galcat/.dat./.ran.}
+          du -h $galcat $rancat
+          tag=QSO_NGC_${version}_${versiono}_truth_all_${imock}
+          ouname=/B/Shared/mehdi/eboss/mocks/${versiono}/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
+          echo ${ouname}
+
+          mpirun -np 16 python $pk --galaxy_path $galcat \
+                                   --random_path $rancat \
+                                   --output_path $ouname \
+                                   --nmesh $nmesh --zlim ${zrange}
+      fi
+      echo 
+      echo
+      echo
+      
+      
+
+      #=================================
+      echo "NN method w "${cont}
+      # NN-based
+      for model in plain known ablation
+      do
+           for zsplit in lowmid all z3
+           do
+               #echo $imock $cont $model $zsplit
+               tag=QSO_NGC_${version}_${versiono}_${model}_${zsplit}_${imock}
+               galcat=/B/Shared/mehdi/eboss/mocks/${versiono}/NGC_${imock}_${cont}/EZmock_eBOSS_${tag}.dat.fits
+               rancat=${galcat/.dat./.ran.}
+               ouname=/B/Shared/mehdi/eboss/mocks/${versiono}/NGC_${imock}_${cont}/pk_${tag}_${cont}_${nmesh}_${zlim}.json
+
+               du -h $galcat $rancat
+               echo ${ouname}
+
+               mpirun -np 16 python $pk --galaxy_path $galcat \
+                                       --random_path $rancat \
+                                       --output_path $ouname \
+                                       --nmesh $nmesh --zlim ${zrange} --sys_tot
+            done
+      done
+      echo 
+      echo
+      echo
+
+
+  done
+done
